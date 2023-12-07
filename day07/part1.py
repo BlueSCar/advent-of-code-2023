e143fd0cfa20c84321d8a489505fabb65d3cfc56
Bill Radjewski <radjewwj@gmail.com>
2023-12-07 10:47:07 -0500

    day 7

---
+++ day07/part1.py
@@ -0,0 +1,56 @@
+from collections import Counter
+from functools import cmp_to_key
+
+CARDS = "23456789TJQKA"
+
+def classify_hand(hand: str):
+    grouped = Counter(hand)
+    ordered = grouped.most_common()
+    uniques = len(ordered)
+    
+    if uniques == 5:
+        return 1
+    elif uniques == 4:
+        return 2
+    elif uniques == 3:
+        if ordered[0][1] == 3:
+            return 4
+        else:
+            return 3
+    elif uniques == 2:
+        if ordered[0][1] == 4:
+            return 6
+        else:
+            return 5
+    else:
+        return 7
+    
+def compare_hands(h1, h2):
+    class_diff = h1['classification'] - h2['classification']
+    if class_diff != 0:
+        return class_diff
+    else:
+        for i in range(5):
+            val_diff = CARDS.index(h1['hand'][i]) - CARDS.index(h2['hand'][i])
+            if val_diff != 0:
+                return val_diff
+    
+    return 0
+
+file = open('day07/input.txt')
+lines = [l.strip().split(' ') for l in file.readlines()]
+
+hands = list()
+
+for l in lines:
+    bid = int(l[1])
+    hand = l[0]
+    classification = classify_hand(hand)
+    
+    hands.append(dict(hand=hand, classification=classification, bid=bid))
+    
+sorted_hands = sorted(hands, key=cmp_to_key(compare_hands))
+
+totals = sum([(i+1)*h['bid'] for i, h in enumerate(sorted_hands)])
+
+print(totals)
